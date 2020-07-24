f2a7f02ade40a50660326a29ae2529c0af959abe
Andrew Wittrock <awittrock@pivotal.io>
2020-07-24 11:30:28 -0700

    First valid failing test
    - proving out the approach of using ginkgo and matchers
    
    Signed-off-by: Paul Warren <pwarren@pivotal.io>

---
+++ tests/ytt/external-db-enabled_test.go
@@ -0,0 +1,27 @@
+package ytt
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("External DB Enabled", func() {
+
+	var templates []string
+
+	BeforeEach(func() {
+		templates = []string{
+			pathToFile("config/postgres"),
+			pathToFile("tests/ytt/postgres/postgres-values.yml"),
+			pathToFile("tests/ytt/postgres/cf-values.yml"),
+		}
+	})
+
+	It("should do something", func() {
+		ctx := NewRenderingContext(templates...).WithData(
+			map[string]string{
+			})
+
+		Expect(ctx).To(ProduceYAML(BeTrue()))
+	})
+})
